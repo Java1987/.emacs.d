1940fb42a122c3910a5cc92cadb1f026c477df21
shiwei <shiwei@shiwei-mba.local>
2016-11-11 15:01:57 +0800

    add something no use

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
+(let ((minver "23.3"))
+  (when (version<= emacs-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version<= emacs-version "24")
+  (message "Your Emacs is old, and some functionality in this config will be disabled. Please upgrade if possible."))
 
+;; disable welcome page
+(setq inhibit-startup-message t)
 
+;; load theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (load-theme 'monokai t)
+
 

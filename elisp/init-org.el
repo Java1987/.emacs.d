2de592cc04135cd77b8b448ecd80353bc9c86a39
Shiwei Zhang <shiwei.zhang@outlook.com>
2018-04-15 13:03:20 +0800

    org-mode问题修复

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -3,8 +3,9 @@
 
 (setq org-src-fontify-natively t)
 
-(setq org-agenda-files "~/Projects/org/")
+;;(setq org-agenda-files "~/Documents/ORG/")
+;;(global-set-key (kbd "C-c a") 'org-agenda)
 
-(global-set-key (kbd "C-c a") 'org-agenda)
+(setq org-export-backends (quote (ascii html icalendar latex md)))
 
 (provide 'init-org)
